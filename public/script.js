59260107a89e3a91752a5fda28bd9456d1565b4a
Castle Crawford <nathanscottcrawford@gmail.com>
2016-05-27 10:32:28 -0500

    login and logout are now functional

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 $(() => {
-  const API_URL = 'https://crud-todo-app-95bd5.firebaseio.com/task'
+  const API_URL = 'https://mycooltodoapp-2f8dc.firebaseio.com'
+  let token = null
+  let userId = null
 
   const addItemToTable = (item, id) => {
     const row = `<tr data-id="${id}">
@@ -13,63 +15,114 @@ $(() => {
     $('tbody').append(row)
   }
 
-  // READ: GET data from firebase and display in table
-  $.get(`${API_URL}.json`)
-    .done((data) => {
-      if (data) {
-        // for (id in data) {
-        //   addItemToTable(data[id])
-        // }
+  const getTasks = () => {
+    $.get(`${API_URL}/${userId}/task.json?auth=${token}`)
+      .done((data) => {
+        if (data) {
+          // for (id in data) {
+          //   addItemToTable(data[id])
+          // }
 
-        Object.keys(data).forEach((id) => {
-          addItemToTable(data[id], id)
-        })
-      }
-      // TODO: handle completed tasks
-    })
+          Object.keys(data).forEach((id) => {
+            addItemToTable(data[id], id)
+          })
+        }
+      })
+  }
 
   // CREATE: form submit event to POST data to firebase
-  $('form').submit((e) => {
-    e.preventDefault(); //// TODO: Make this not refresh the page
+  $('.new form').submit((e) => {
     // $.ajax({
     //   url: `${API_URL}.json`,
     //   method: 'POST',
     //   data: JSON.stringify({ task: 'I was posted!' })
     // })
-    $.post(`${API_URL}.json`,
-      JSON.stringify({ task: $(".form-control").val() })
+
+    $.post(`${API_URL}/${userId}/task.json?auth=${token}`,
+      JSON.stringify({ task: 'I was posted!' })
     )
 
+    // TODO: Grab the form text
+    // TODO: Make this not refresh the page
   })
 
   $('tbody').on('click', '.delete', (e) => {
     const row =  $(e.target).closest('tr')
-    const id = row.data('id')
+    const taskId = row.data('id')
 
     $.ajax({
-      url: `${API_URL}/${id}.json`,
+      url: `${API_URL}/${userId}/task/${taskId}.json?auth=${token}`,
       method: 'DELETE'
     }).done(() => {
       row.remove()
-
-    })
-
-  $('tbody').on('click', '.complete', (e) => {
-    const row =  $(e.target).closest('tr')
-    const id = row.data('id')
-
-    $.ajax({
-      url: `${API_URL}/${id}.json`,
-      method: 'PATCH  '
-    }).done(() => {
-      row.add()
-
     })
   })
-})
-})
 
-// TODO:
-// DELETE: click event on delete to send DELETE to firebase.
-// UPDATE: click event on complete to send PUT/PATCH to firebase
+  firebase.initializeApp({
+   apiKey: "AIzaSyD7OHkXVKNz6h7uJXkN67n-VblLweEQ-wA",
+    authDomain: "mycooltodoapp-2f8dc.firebaseapp.com",
+    databaseURL: "https://mycooltodoapp-2f8dc.firebaseio.com",
+    storageBucket: "mycooltodoapp-2f8dc.appspot.com",
+  })
 
+  // both return promise like objects
+  const login = (email, password) => (
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+  )
+
+  const register = (user, password) => (
+    firebase.auth().createUserWithEmailAndPassword(user, password)
+  )
+
+  $('.login form').submit((e) => {
+    const form = $(e.target)
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    login(email, password)
+      .then(console.log)
+      .catch(console.err)
+
+    e.preventDefault()
+  })
+
+  $('input[value="Register"]').click((e) => {
+    const form = $(e.target).closest('form')
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    register(email, password)
+      .then(() => login(email, password))
+      .then(console.log)
+      .catch(console.err)
+
+    e.preventDefault()
+  })
+
+  $('.logout').click(() => {
+    firebase.auth().signOut()
+  })
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // logged in
+      $('.login').hide()
+      $('.app').show()
+
+      $('.logged_in_user').text(user.email)
+
+      userId = user.uid
+
+      user.getToken()
+        .then(t => token = t)
+        .then(getTasks)
+
+    } else {
+      // logged out
+      $('.app').hide()
+      $('.login').show()
+      $('tbody').empty()
+    }
+  })
+})
